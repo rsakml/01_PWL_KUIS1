e7b46eb8e02b5faa306d486dcd8cbc3bc1459d45
= <=>
2022-03-16 00:24:27 +0700

    update DatabaseSeeder call PegawaiSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(SupplierSeeder::class);
+        // $this->call(SupplierSeeder::class);
+        $this->call(PegawaiSeeder::class);
     }
 }
